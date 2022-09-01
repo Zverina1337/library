e0e4344329747618b08bf63816c61dcc736328de
Zverina <danii.zvorugin@gmail.com>
2022-09-01 14:56:06 +0700

    Task_8

--- React/task_8/my-app/src/App.js
+++ React/task_8/my-app/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import {v4} from 'uuid';
 
 function App() {
+
+  const initProds = [
+    {id: v4(), name: 'prod1', catg: 'catg1', cost: 100},
+    {id: v4(), name: 'prod2', catg: 'catg2', cost: 200},
+    {id: v4(), name: 'prod3', catg: 'catg3', cost: 300},
+  ];
+  const table = initProds.map(table => {
+      return (
+        <table>
+          <tBody>
+            <tr>
+              <td>{table.name}</td>
+              <td>{table.catg}</td>
+              <td>{table.cost}</td>
+            </tr>
+          </tBody>
+        </table>)
+        
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {table}
     </div>
   );
 }
